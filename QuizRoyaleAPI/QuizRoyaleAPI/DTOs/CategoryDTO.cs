33643a8510a4e950213021234249b881c4ffc92e
David-dev8 <72741163+David-dev8@users.noreply.github.com>
2022-05-28 13:22:07 +0200

    Implementatie van QuestionService

--- QuizRoyaleAPI/QuizRoyaleAPI/DTOs/CategoryDTO.cs
+++ QuizRoyaleAPI/QuizRoyaleAPI/DTOs/CategoryDTO.cs
@@ -4,14 +4,17 @@ namespace QuizRoyaleAPI.Models
 {
     public class CategoryDTO
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Color { get; set; }
 
         public string Picture { get; set; }
 
-        public CategoryDTO(string name, string color, string picture)
+        public CategoryDTO(int id, string name, string color, string picture)
         {
+            Id = id;
             Name = name;
             Color = color;
             Picture = picture;
