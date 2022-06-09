ab59c7d4afe5aa7f6745e701934201528810c8e6
David-dev8 <72741163+David-dev8@users.noreply.github.com>
2022-06-09 11:13:34 +0200

    Nieuwe migratie

--- QuizRoyaleAPI/QuizRoyaleAPI/Program.cs
+++ QuizRoyaleAPI/QuizRoyaleAPI/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddDataServices();
 // JWT Tokens
 builder.Services.AddJWT(builder.Configuration.GetSection("Authentication:Key").Value);
 
+builder.WebHost.UseKestrel().UseIIS().UseIISIntegration().UseSetting("https_port", "5001").UseSetting("http_port", "5000");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
